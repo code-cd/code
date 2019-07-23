9788e1d5665178d57848b06ce3ff9e6511eec759
cdipi <117311444@qq.com>
2019-07-23 11:11:42 +0800

    新建main测试dao,TestDay01代码提交

--- src/com/cd/dao/DaoTest.java
+++ src/com/cd/dao/DaoTest.java
@@ -1,4 +1,7 @@
 package com.cd.dao;
 
 public class DaoTest {
+	public static void main(String[] args) {
+		System.out.println("测试dao方法提交");
+	}
 }
--- src/com/cd/service/TestDay01.java
+++ src/com/cd/service/TestDay01.java
@@ -1,4 +1,7 @@
 package com.cd.service;
 
 public class TestDay01 {
+	public static void main(String[] args) {
+		System.out.println("开始测试提交");
+	}
 }
