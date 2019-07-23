0716055dd7c4778173793f4231ca90d9f69d4164
cdipi <117311444@qq.com>
2019-07-23 14:24:21 +0800

    这是2.0之上开发测试

--- src/com/cd/serviceImpl/TestImpl.java
+++ src/com/cd/serviceImpl/TestImpl.java
@@ -5,6 +5,8 @@ package com.cd.serviceImpl;
  */
 public class TestImpl {
 	public static void main(String[] args) {
+		
 		System.out.println("master2.0版本开发测试");
 	}
+
 }
---
+++ src/com/cd/serviceImpl/ToString.java
@@ -0,0 +1,8 @@
+package com.cd.serviceImpl;
+
+public class ToString {
+	public static void main(String[] args) {
+	System.out.println("这是2.0之上开发测试");
+
+	}
+}
